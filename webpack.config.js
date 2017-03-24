20bd33c8cc7d2baf297f91ec48e3706a1abdb7a1
goseeya <149542@gmail.com>
2017-03-24 19:21:04 +0200

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
             {
                 test: /\.scss$/,
                 loader: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
             }
         ]
     }
